a4fadcd203c885201fdf18b2c9c6c8afe6e02d46
Dhananjay <sonardhananjay@gmail.com>
2018-07-11 18:42:19 +0530

    Git commit python project to local

---
+++ src/newpythonproject.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "DH004SO"
+__date__ = "$11 Jul, 2018 6:30:14 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
